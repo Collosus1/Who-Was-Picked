87c30428188d19969ffe0e14a3986fcb5d17fbba
Collosus1 <93479164+Collosus1@users.noreply.github.com>
2022-06-05 08:28:16 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var voteLog = [];
+
+function doAlert() {
+  alert('Why did you click thatsdfasdfs');
+};
+
+
+function votingCount(){
+  setTimeout(()=> alert('You chose Person1'),300);
+  voteLog.push('Person1');
+};
